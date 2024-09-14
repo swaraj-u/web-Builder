7af273f053a7ae6af7ba2b82cb280564dea0c857
harshit-kms <kmsharshit@gmail.com>
2024-09-14 23:08:06 +0530

    initial

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,38 @@
 const express= require('express');
+const http = require("http");
 const dotenv= require('dotenv');
 const connectDB = require('./config/db');
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
-const app=express();
-const PORT = process.env.PORT || 5000;
-app.listen(5000,console.log(`Server started on ${PORT}`));
 
-app.get('/',(req,res)=>{
-    res.send('Api is running!');
+const app=express();
+app.use(cors());
+
+//Creating http server using express
+const server = http.createServer(app);
+
+//Setting up socket.io with cors
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", // origin of front-end (REACT)
+        method: ["GET", "POST"],
+    },
 });
+
+io.on("connection", (socket) => {
+    console.log(`user connected ${socket.id}`);
+
+    socket.on("addTask", (data) => {
+        socket.broadcast.emit("receiveTask", data );
+    });
+
+
+
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(5000,console.log(`Server started on ${PORT}`));
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
-
+import { useEffect, useState } from "react"; 
+import io from 'socket.io-client'; 
+const socket = io.connect("http://localhost:5000"); // connection with backend
 
 function App() {
+
+  const [task, setTask] = useState("");
+  const [taskReceived, setTaskReceived] = useState("");
+ 
+  const addTask = () => {
+    socket.emit("addTask", {task})
+  };
+
+  useEffect(() => {
+    socket.on("receiveTask", (data) => {
+      setTaskReceived(data.task);
+    })
+  }, [socket]);
   return (
     <div className="App">
-      hello
+      <input placeholder="Task" onChange={(event) => {
+        setTask(event.target.value);
+      }} />
+      <button onClick={addTask}>Add Task</button>
+      <h1>Task</h1>
+      {taskReceived}
     </div>
   );
 }
